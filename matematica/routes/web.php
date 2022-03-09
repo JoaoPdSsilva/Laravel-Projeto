2eea541c020f13dcb6b4ac5e211a209e24ed5c41
JoaoPdSsilva <joaopedro123jp1@outlook.com>
2022-03-09 09:56:07 -0300

    Criação de novas rotas com mais parametros

--- matematica/routes/web.php
+++ matematica/routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Route::get('/Joao', function () {
     echo "Meu é João Pedro e estou estudando laravel";
 });
+
+Route::get('/tabuada/{num}', function ($num) {
+    echo "<h1>Tabuada do número $num </h1>";
+    for ($i = 0; $i <= 10; $i++){
+        echo "<h3>$i X $num = ".$i*$num."</h3>";
+    }
+});
+
+Route::get('/usuario/{nome}/{sobrenome}', function ($nome, $sobrenome) {
+    echo "<h1>Olá $nome $sobrenome !</h1>";
+});
